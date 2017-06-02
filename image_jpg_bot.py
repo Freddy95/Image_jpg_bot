056b93d4624fcafd2816dc3e9a04d1318cef8b24
Festevez <f.estevez95@gmail.com>
2017-06-01 21:34:19 -0400

    correctly retrieves replies and response to the add command

--- image_jpg_bot.py
+++ image_jpg_bot.py
@@ -12,16 +12,21 @@ def bot_login():
 	return r
 #check for images that can be linked
 def run(r, comments_replied_to, add_pattern):
-	for comment in r.subreddit('test').comments(limit=25):
-		if comment.id not in comments_replied_to and comment.author != r.user.me():
-			if comment.body in images:
+	print comments_replied_to
+	for comment in r.subreddit('test').comments(limit=70):
+		#check if comment is one we should check
+		if comment.id not in comments_replied_to:
+			if comment.author == r.user.me():
+				comment.refresh()
+				comment.replies.replace_more()
+				for reply in comment.replies:
+					print reply.body
+					if add_pattern.match(reply.body) and reply.id not in comments_replied_to:
+						reply.reply("added -> " + reply.body)
+						add_to_replied_comments(reply, comments_replied_to)
+			elif comment.body in images:
 				comment.reply("[" + comment.body + "](" + images[comment.body] + ")")
-				comments_replied_to.append(comment.id)
-				with open("comments_replied_to.txt", "a") as f:
-					f.write(comment.id + "\n")
-			#user may want to add a new link
-			elif add_pattern.match(comment.body):
-				print comment.body
+				add_to_replied_comments(comment, comments_replied_to)
 			
 
 #Retrieve the saved comments from the comments_replied_to.txt file
@@ -33,6 +38,11 @@ def get_saved_comments():
 			comments_replied_to = f.read()
 			comments_replied_to = comments_replied_to.split("\n")
 	return comments_replied_to
+#add comment to list of replied comments
+def add_to_replied_comments(comment, comments_replied_to):
+	comments_replied_to.append(comment.id)
+	with open("comments_replied_to.txt", "a") as f:
+		f.write(comment.id + "\n")
 
 comments_replied_to = get_saved_comments()
 images = {"feelsbadman.jpg" : "http://imgur.com/buEBNnk", "iseewhatyoudidthere.jpg" : "http://imgur.com/9gBzlFv"}
